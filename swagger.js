9d88da78e1bc540ff465b4a5af1c9acbc5fa6ecc
Oluwafunmilayo Owolabi <damilolaowolabi55@gmail.com>
2025-10-19 15:05:08 +0100

    Updated routes and Swagger setup

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "https://todo-api-documentation.onrender.com",
+        url: "https://todo-api-documentation.onrender.com/api-docs/",
       },
     ],
     
